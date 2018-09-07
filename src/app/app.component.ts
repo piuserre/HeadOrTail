3b4696c7f0d7a2222b370d0b7598b7bfefcdf382
Giuseppe Pierri <giuseppe.pierri@ericsson.com>
2018-09-07 17:40:49 +0200

    Update Title in App-Component
      - modify title in html app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'headOrTail';
+  title = 'Head or Tail';
   throw = -1;
 
   listThrows = [];

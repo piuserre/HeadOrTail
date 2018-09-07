2b8d098d211641edc7238d09576d0868cf022f82
Giuseppe Pierri <giuseppe.pierri@ericsson.com>
2018-09-07 17:28:17 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'headOrTail';
+  throw = -1;
+
+  listThrows = [];
+
+  throws(): void {
+    this.throw = -1;
+    
+    
+
+    async function delay(ms: number) {
+      await new Promise(resolve => setTimeout(()=>resolve(), 1000)).then(()=>console.log("fired"));
+    }
+    delay(1000);
+
+    this.throw = Math.round(Math.random());
+    this.listThrows.push(this.throw);
+  }
+
+  format(): void {
+    this.listThrows = [];
+  }
 }

7865879a685f0924df49251fd2c9de0dbf622af3
piuserre <giuseppe.pierri@yahoo.com>
2018-09-07 21:51:31 +0200

    Set Timeout with arrow function
      - update app-component.t

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,12 @@ export class AppComponent {
 
   throws(): void {
     this.throw = -1;
-    async function delay(ms: number) {
-      await new Promise(resolve => setTimeout(()=>resolve(), 1000)).then(()=>console.log("fired"));
-    }
-    delay(1000);
+    setTimeout(()=>{
+      this.throw = Math.round(Math.random());
+      this.listThrows.push(this.throw);
+    }, 3000);
 
-    this.throw = Math.round(Math.random());
-    this.listThrows.push(this.throw);
+    
   }
 
   format(): void {
